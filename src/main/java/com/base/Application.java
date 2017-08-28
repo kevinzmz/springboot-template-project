1c81756fb8903454a531a14078987b6adef3353c
kevin <zhangmuzhu@yeah.net>
2017-08-28 17:51:25 +0800

    update files

--- src/main/java/com/base/Application.java
+++ src/main/java/com/base/Application.java
@@ -1,19 +1,9 @@
 package com.base;
 
-import com.base.services.Receiver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-
-import java.util.concurrent.CountDownLatch;
 
 /**
  * <P></P>
@@ -23,45 +13,7 @@ import java.util.concurrent.CountDownLatch;
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @Bean
-    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }
-
-    @Bean
-    Receiver receiver(CountDownLatch latch) {
-        return new Receiver(latch);
-    }
-
-    @Bean
-    CountDownLatch latch() {
-        return new CountDownLatch(1);
-    }
-
-    @Bean
-    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
-    }
-
     public static void main(String[] args) throws InterruptedException {
-        //SpringApplication.run(Application.class,args);
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-        CountDownLatch latch = ctx.getBean(CountDownLatch.class);
-
-        logger.info("Sending message...");
-        template.convertAndSend("chat", "hello from redis!");
-
-        latch.await();
-
-        System.exit(0);
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/base/controller/BaseController.java
+++ src/main/java/com/base/controller/BaseController.java
@@ -1,12 +1,8 @@
 package com.base.controller;
 
-import org.springframework.stereotype.Controller;
-
 /**
  * <P></P>
  * Created by kevin on 2017/8/2 14:29.
  */
-@Controller
 public class BaseController {
-
 }
