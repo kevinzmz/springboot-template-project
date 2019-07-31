5ac83f57a2470f9b61b9349cdd9967ef191e2bd7
kevin <zhangmuzhu@yeah.net>
2019-07-31 14:30:31 +0800

    增加socket nio调用样例

---
+++ src/main/java/com/base/test/TestSocketClientNio.java
@@ -0,0 +1,57 @@
+package com.base.test;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author kevin
+ * @version V1.0
+ * @Description: TODO
+ * @date 2019/7/31 11:52
+ **/
+public class TestSocketClientNio {
+    public static void client(){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("10.4.81.59",9000));
+            if(socketChannel.finishConnect()){
+                int i = 0;
+                while(true){
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = "I’am " + i++ + "-th information from client";
+                    byteBuffer.clear();
+                    byteBuffer.put(info.getBytes());
+                    byteBuffer.flip();
+                    while(byteBuffer.hasRemaining()){
+                        System.out.println(byteBuffer);
+                        socketChannel.write(byteBuffer);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            if(socketChannel!=null){
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestSocketClientNio.client();
+    }
+}
---
+++ src/main/java/com/base/test/TestSocketServerNio.java
@@ -0,0 +1,59 @@
+package com.base.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @author kevin
+ * @version V1.0
+ * @Description: TODO
+ * @date 2019/7/31 13:53
+ **/
+public class TestSocketServerNio {
+    public static void server(){
+        ServerSocket serverSocket = null;
+        InputStream in = null;
+
+        try {
+            serverSocket = new ServerSocket(9000);
+            int recvMsgSize = 0;
+            byte[] recvBuf = new byte[1024];
+            while(true){
+                Socket clntSocket = serverSocket.accept();
+                SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
+                System.out.println("Handling client at " + clientAddress);
+                in = clntSocket.getInputStream();
+                while((recvMsgSize=in.read(recvBuf))!=-1){
+                    byte[] temp = new byte[recvMsgSize];
+                    System.arraycopy(recvBuf,0,temp,0,recvMsgSize);
+                    System.out.println(new String(temp));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(serverSocket!=null){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(in!=null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestSocketServerNio.server();
+    }
+}
