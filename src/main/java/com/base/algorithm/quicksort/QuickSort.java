27e109e0ed105f35ceee55c7d4b13099a86db851
zhangmuzhu <zhangmuzhu@yeah.net>
2020-06-29 22:11:10 +0800

    增加样例

--- src/main/java/com/base/algorithm/quicksort/QuickSort.java
+++ src/main/java/com/base/algorithm/quicksort/QuickSort.java
@@ -2,13 +2,24 @@ package com.base.algorithm.quicksort;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Random;
+
 /**
  * @auther kevin
  * @date 2020/6/29 16:10
  */
 public class QuickSort {
     public static void quickSort(int []a,int i,int j){
+        Random r = new Random();
         int n = j;
+
+        if(j>i){
+            int ran = r.nextInt(j-i) + i;
+            int tempRan = a[n];
+            a[n] = a[tempRan];
+            a[tempRan] = tempRan;
+        }
+
         if(i < j-1){
             n = j;
             int t = i-1;
@@ -38,9 +49,32 @@ public class QuickSort {
         }
     }
 
+    public static void quickSortSec(int []a,int left,int right){
+        int i=left;
+        int j=right;
+        int temp = a[left];
+        while(left < right){
+            while(temp < a[right]){
+                right--;
+            }
+                a[left] = a[right];
+            while(temp > a[left]){
+                left++;
+            }
+            a[right] = a[left];
+        }
+        a[left] = temp;
+        quickSort(a,i,left-1);
+        quickSort(a,left+1,j);
+    }
+
     public static void main(String[] args) {
         int []input = {4,1,6,2,7,8,2,9,0,7};
         quickSort(input,0,input.length-1);
         System.out.println(JSON.toJSONString(input));
+
+        int []sec= {4,1,6,2,7,8,2,9,0,7};
+        quickSort(sec,0,sec.length-1);
+        System.out.println(JSON.toJSONString(sec));
     }
 }
